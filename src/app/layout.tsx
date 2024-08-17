66d036de70669a5c796f0976f626d07c79267263
James Mccarthy <mccarthyjames35@yahoo.com>
2024-08-16 20:47:20 -0700

    react validation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,19 +5,19 @@ import { ReactNode } from 'react';
 import TopNav from './components/navbar/TopNav';
 
 export const metadata: Metadata = {
-  title: 'DatePulse',
-  description: 'DatePulse is a simple app to manage your matches'
+    title: 'DatePulse',
+    description: 'DatePulse is a simple app to manage your matches'
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang='en'>
-      <body>
-        <Providers>
-          <TopNav />
-          <main className='container mx-auto p-10'>{children}</main>
-        </Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body>
+                <Providers>
+                    <TopNav />
+                    <main className='container mx-auto'>{children}</main>
+                </Providers>
+            </body>
+        </html>
+    );
 }
